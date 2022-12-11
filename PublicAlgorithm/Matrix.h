9c157c3294f815a0abd59149a12ec3e9a0bdde7c
LoveCyy123 <824107224@qq.com>
2022-12-11 20:51:17 +0800

    add martix.h without relize

---
+++ PublicAlgorithm/Matrix.h
@@ -0,0 +1,26 @@
+#ifndef MATRIX_H
+#define MATRIX_H
+
+#include <iostream>
+
+namespace OpenCV{
+
+class Matrix{
+    https://blog.csdn.net/xholes/article/details/79966211
+
+    https://blog.csdn.net/nameofcsdn/article/details/120968236?spm=1001.2101.3001.6650.1&utm_medium=distribute.pc_relevant.none-task-blog-2%7Edefault%7EBlogCommendFromBaidu%7ERate-1-120968236-blog-79966211.pc_relevant_3mothn_strategy_and_data_recovery&depth_1-utm_source=distribute.pc_relevant.none-task-blog-2%7Edefault%7EBlogCommendFromBaidu%7ERate-1-120968236-blog-79966211.pc_relevant_3mothn_strategy_and_data_recovery&utm_relevant_index=2
+public:
+
+
+
+
+private:
+
+
+
+
+};
+
+}
+
+#endif

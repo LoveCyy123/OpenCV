c6f7db1896986bfad54996e3c3f4e3e50adeb643
LoveCyy123 <824107224@qq.com>
2022-12-12 18:31:27 +0800

    add class point2d and point3d and PublicApi.h

---
+++ PublicAlgorithm/Matrix.cpp
@@ -0,0 +1,32 @@
+#include Matrix.h
+
+namespace CV{
+
+//默认构造函数
+Matrix::Matrix(){
+
+}
+
+//指定类型的构造函数
+Matrix::Matrix(int rows, int cols, int type){
+
+
+}
+
+//指定类型和初始化值的构造函数
+Matrix::Matrix(int rows, int cols, int type, const Scalar&s){
+
+
+}
+
+//指定类型的二维数组，并且将具体的数据储存其中
+Matrix::Matrix(int rows, int cols, int type, void* data, size_t step = AUTO_STEP){
+
+    
+}
+
+
+
+
+
+}
---
+++ PublicAlgorithm/PublicApi.h
@@ -0,0 +1,16 @@
+#ifndef PUBLICAPI_H
+#define PUBLICAPI_H
+
+#include "Matrix.h"
+
+namespace CV{
+
+static Matrix ImRead(&argc);
+
+static Matrix ImShow(&argc);
+
+static Matrix WaitKey(&argc);
+
+}//namespace CV
+
+#endif
